a1b1ac68fb48b313649a517bf3712a4717f0c1e4
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2023-10-24 21:42:14 -0500

    Initial commit

---
+++ app/src/main/java/com/dado/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById<Button>(R.id.roll_button)
+        val resultText: TextView = findViewById<TextView>(R.id.result_text)
+        rollButton.setOnClickListener {
+            val randomInt = (1..6).random()
+            resultText.text = randomInt.toString()
+        }
+    }
+}
